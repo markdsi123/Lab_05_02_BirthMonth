c660d2042f1963fe89eb643275dcdd05cb94757d
markd <dsilvamj@mail.uc.edu>
2022-09-25 22:03:14 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int month;
+        month = 8;
+
+        if (month>=1 && month<=12)
+            System.out.println("your birth month number is - " + month);
+
+        else
+            System.out.println("the birth number entered " + month + " is incorrect");
+
+
+
+    }
+}
